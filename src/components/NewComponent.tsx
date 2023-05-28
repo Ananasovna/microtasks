a2b0d2a014cf292f34546ba6e7b178c6c986b0c9
Ananasovna <a.n.ushkova@yandex.ru>
2023-05-28 22:57:26 +0200

    feat: add 1.1 microtask

--- src/components/NewComponent.tsx
+++ src/components/NewComponent.tsx
@@ -1,8 +1,30 @@
 
 export const NewComponent = () => {
-    return (
-        <div>
 
-        </div>
+    const topCars = [
+        {manufacturer:'BMW', model:'m5cs'},
+        {manufacturer:'Mercedes', model:'e63s'},
+        {manufacturer:'Audi', model:'rs6'}
+    ]
+
+    return (
+        <table>
+            <tr>
+                <th>Number</th>
+                <th>Car</th>
+                <th>Model</th>
+            </tr>
+            {topCars.map((el, index) => {
+                return (
+                    <tr>
+                        <td>{index + 1}</td>
+                        <td>{el.manufacturer}</td>
+                        <td>{el.model}</td>
+                    </tr>
+                )
+            })
+            }
+
+         </table>
     )
 }
